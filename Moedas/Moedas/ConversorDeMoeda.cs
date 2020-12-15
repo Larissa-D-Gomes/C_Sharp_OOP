58562492d54e1e7bc3a403e727ccf9dfe37864ac
Larissa-D-Gomes <larissadgomes2001@gmail.com>
2020-12-15 10:27:41 -0200

    Conversor Dólar/Real

---
+++ Moedas/Moedas/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moedas
+{
+    class ConversorDeMoeda
+    {
+        private static double IOF = 6.0;
+        
+        public static double Total(double cotacao, double quantidade)
+        {
+            double p = quantidade * cotacao;
+            return p + p * IOF / 100;
+        }
+    }
+}
---
+++ Moedas/Moedas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quant = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valor = ConversorDeMoeda.Total(cotacao, quant);
+            Console.Write("Valor a ser pago em reais = " + valor.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
