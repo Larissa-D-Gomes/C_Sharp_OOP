2b755f58b858c432264d7251428c09e3463a5566
Larissa-D-Gomes <larissadgomes2001@gmail.com>
2020-12-17 16:14:36 -0200

    Projeto conta bancária utilizando properties em atributos da classe

---
+++ Property/Property/Conta.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Property
+{
+    class Conta
+    {
+        public double Saldo { get; private set; }
+        private string _nome;
+        public int NumeroCont { get; private set; }
+
+        //Construtores
+
+        /* Construtor atributos obrigatórios
+         * @param string nome, int numeroCont
+         */
+        public Conta(string nome, int numeroCont)
+        {
+            this._nome = nome;
+            this.NumeroCont = numeroCont;
+        }
+
+        /* Construtor atributos obrigatorios e saldo inicial 
+         * @param string nome, int numeroCont, double saldo
+         */
+        public Conta(string nome, int numeroCont, double saldo) : this(nome, numeroCont)
+        {
+            this.Saldo = saldo;
+        }
+
+        // Propriedades 
+        public string Nome
+        {
+            get
+            {
+                return this._nome;
+            }
+            set
+            {
+                if (value != null && value.Length > 1)
+                    this._nome = value;
+            }
+        }
+
+
+        // Métodos
+
+        /*Método para sacar*/
+        public void sacar(double valor)
+        {
+            if (valor > 0.0)
+                this.Saldo = Saldo - valor - 5;
+        }
+
+        /*Método para depositar*/
+        public void depositar(double valor)
+        {
+            if (valor > 0.0)
+                this.Saldo += valor;
+        }
+        public override string ToString()
+        {
+            return "Conta: " + this.NumeroCont
+                   + ", Titular: " + this.Nome + ", Saldo: $ " 
+                   + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Property/Property/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Property
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double aux;
+            Conta c;
+            Console.Write("Entre o número da conta: ");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string resp = Console.ReadLine();
+            
+            if(resp.ToLower().Equals("s"))
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                aux = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new Conta(nome, num, aux);
+            } 
+            else
+            {
+               c = new Conta(nome, num);
+            }
+
+            Console.WriteLine("\nDados da conta:\n" + c + "\n");
+
+            Console.Write("Entre um valor para depósito: ");
+            aux = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.depositar(aux);
+            Console.WriteLine("\nDados da conta atualizados:\n" + c + "\n");
+
+            Console.Write("Entre um valor para saque: ");
+            aux = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.sacar(aux);
+            Console.WriteLine("\nDados da conta atualizados:\n" + c + "\n");
+        }
+    }
+}
