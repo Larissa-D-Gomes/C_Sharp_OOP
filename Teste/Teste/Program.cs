7347e483d4575ec29b6685c3ed01cc4854853e92
Larissa Gomes <larissadgomes2001@gmail.com>
2020-12-24 13:28:43 -0300

    Teste gitignore

---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "1234";
+            Console.WriteLine(s.IndexOf("1"));
+        }
+    }
+}
