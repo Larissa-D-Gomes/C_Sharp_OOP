dfebe3b4de4ecc8e94b23befd6ef21bfb35ba27b
Larissa Gomes <larissadgomes2001@gmail.com>
2021-01-03 14:43:13 -0300

    Projeto Bank (Exception)

---
+++ Bank/Bank/Entities/Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using Bank.Entities.Exceptions;
+
+namespace Bank.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+        public double WithdrawLimit { get; private set; }
+
+        // Constructor
+        // @param int number, string holder, double balance, double withdrawLimit
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            this.Number = number;
+            this.Holder = holder;
+            this.Balance = balance;
+            this.WithdrawLimit = withdrawLimit;
+        }
+
+        // Methods
+
+        /* Deposit method
+         * @param double amount
+         */
+        public void Deposit(double amount)
+        {
+            if (amount <= 0.0)
+                throw new DomainException("Deposit error: Invalid deposit amount");
+
+            this.Balance += amount;
+        }
+
+        /* Withdraw method
+         * @param double amount
+         */
+        public void Withdraw(double amount)
+        {
+            if (amount > this.WithdrawLimit)
+                throw new DomainException("Withdraw error: The amount exceeds withdraw limit");
+
+            if (amount > this.Balance)
+                throw new DomainException("Withdraw error: Not enough balance");
+
+
+            this.Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return   "Number: " + this.Number
+                   + "Holder: " + this.Holder
+                   + "Balance: " + this.Balance.ToString("F2", CultureInfo.InvariantCulture)
+                   + "Withdraw limit: "
+                   + this.WithdrawLimit.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using Bank.Entities;
+using Bank.Entities.Exceptions;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+
+                Console.Write("Number: ");
+                int n = int.Parse(Console.ReadLine());
+
+                Console.Write("Holder: ");
+                string h = Console.ReadLine();
+
+                Console.Write("Initial balance: ");
+                double ib = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.Write("Withdraw limit: ");
+                double wl = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account ac = new Account(n, h, ib, wl);
+
+                Console.Write("\nEnter amount for withdraw: ");
+                double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                ac.Withdraw(a);
+                Console.WriteLine("New balance: " + ac.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error in format: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
+
+        }
+    }
+}
