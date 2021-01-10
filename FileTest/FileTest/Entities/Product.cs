d1c1cea8b52446d52be90a4a89fe0f2d58d4d836
Larissa-D-Gomes <larissadgomes2001@gmail.com>
2021-01-10 14:22:30 -0300

    Manipulação de arquivos e diretórios

---
+++ FileTest/FileTest/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileTest.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return this.Price * this.Quantity;
+        }
+    }
+}
---
+++ FileTest/FileTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using FileTest.Entities;
+
+namespace FileTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] s = File.ReadAllLines(sourceFilePath);
+
+                string folderPath = Path.GetDirectoryName(sourceFilePath);
+                
+                Directory.CreateDirectory(folderPath + @"\Out");
+                
+                using (StreamWriter sw = File.AppendText(folderPath + @"\Out\summary.csv"))
+                {
+                    foreach(string i in s)
+                    {
+                        string[] aux = i.Split(',');
+                        Product p = new Product(aux[0], double.Parse(aux[1], CultureInfo.InvariantCulture), int.Parse(aux[2]));
+                        sw.WriteLine(p.Name + "," + p.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
