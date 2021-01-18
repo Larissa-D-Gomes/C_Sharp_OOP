a4b684d094bd00549c831539297169293ce3738a
Larissa-D-Gomes <larissadgomes2001@gmail.com>
2021-01-18 14:02:09 -0300

    Conjuntos Dictionary

---
+++ VoteCounter/VoteCounter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace VoteCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            Dictionary<string, int> votes = new Dictionary<string, int>();
+            
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] s = sr.ReadLine().Split(',');
+                        
+                        if(votes.ContainsKey(s[0]))
+                            votes[s[0]] += int.Parse(s[1]);
+                        else
+                            votes[s[0]] = int.Parse(s[1]);
+                    }
+
+                    foreach (var i in votes)
+                        Console.WriteLine(i.Key + ": " + i.Value);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
